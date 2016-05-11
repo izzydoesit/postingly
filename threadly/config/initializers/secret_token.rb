697f9da8fc8de1c3e0caa7039a2a43562689dc75
Israel Matos <israeldmatos@gmail.com>
2016-05-10 17:22:09 -0700

    added data persistence, postgresql

---
+++ threadly/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Threadly::Application.config.secret_key_base = '493f8407b5c82d548e38f081367e881211098df72f66504c07b208ad07ce8db567d24b6dd5dd24d2ffabb48e070d825caf10de65a4eb1fcef288a5a5b2fab00f'
